c4d57c4bd5b0cd44bab2f6298545226e21632399
EmanuelMartinez23 <emanuelmartinez230801@gmail.com>
2024-12-21 02:45:02 -0600

    Chapter 5 : Serializers- POST, GET, PUT and DELETE Request

--- watchlist_app/api/serializers.py
+++ watchlist_app/api/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from watchlist_app.models import Movie
 
 # Serializador para el modelo Movie
 class MovieSerializer(serializers.Serializer):
@@ -7,3 +8,14 @@ class MovieSerializer(serializers.Serializer):
     name = serializers.CharField()
     description = serializers.CharField()
     activate = serializers.BooleanField()
+
+    # metodos para las solicitudes POST para crear un object
+    def create(self, validated_data):
+        return Movie.objects.create(**validated_data)
+    
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.description = validated_data.get('description', instance.description)
+        instance.activate = validated_data.get('activate', instance.activate)
+        instance.save()
+        return instance
--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -2,17 +2,60 @@ from watchlist_app.models import Movie
 from watchlist_app.api.serializers import MovieSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import status
 
 # view para regresar la lista de peliculas
 @api_view(['GET', 'POST'])
 def movie_list(request):
-    movies = Movie.objects.all()
-    serializer = MovieSerializer(movies, many = True)
-    return Response(serializer.data)
+    # diferencia que metodo es
+    if request.method == 'GET':
+        movies = Movie.objects.all()
+        serializer = MovieSerializer(movies, many = True)
+        return Response(serializer.data, status = status.HTTP_200_OK)
+    if request.method == 'POST':
+        # se crea el archivo
+        serializer = MovieSerializer(data = request.data)
+        # si es valido que se guarde y retornamos
+        if serializer.is_valid():
+            serializer.save() 
+            return Response(serializer.data)
+        else: 
+            return Response(serializer.errors)
+
+
 
 # view para ver el detalle de una pelicula
-@api_view()
+@api_view(['GET', 'PUT', 'DELETE'])
 def movie_details(request, pk):
-    movie = Movie.objects.get(pk = pk)
-    serializer = MovieSerializer(movie)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        try:
+            movie = Movie.objects.get(pk = pk)
+        except Movie.DoesNotExist:
+            return Response(
+                {'error': 'Movie not found'},
+                status=status.HTTP_404_NOT_FOUND
+        )
+        serializer = MovieSerializer(movie)
+        return Response(
+            serializer.data,
+            status = status.HTTP_200_OK)
+    # modificar registro
+    if request.method == 'PUT':
+        # necesitamos pasarle que registro modifica
+        movie = Movie.objects.get(pk = pk)
+        serializer = MovieSerializer(movie ,data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(
+                serializer.errors,
+                status = status.HTTP_400_BAD_REQUEST
+                )
+
+# borramos
+    if request.method == 'DELETE':
+        movie = Movie.objects.get(pk = pk)
+        movie.delete()
+        # se borra y mandamos que ya no hay contenido
+        return Response(status= status.HTTP_204_NO_CONTENT)
